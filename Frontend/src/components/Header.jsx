37b3ee874b50f1464987faf9a4f03797472f8bf4
Jack <jackwebdev31@gmail.com>
2023-10-09 12:04:02 +0200

    toastify buggy

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,8 +1,27 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/userApiSlice";
+import { logout } from "../slices/authSlice";
 
 function Header() {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const handleLogOut = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   function handleToggle() {
     setToggleMenu((prev) => !prev);
@@ -20,21 +39,40 @@ function Header() {
               : "mobile grid gap-[1.5rem] text-center text-white bg-red-700 -translate-y-[150%] -translate-x-[2rem] py-[2rem] w-full md:hidden absolute"
           }
         >
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
+          {userInfo ? (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <div onClick={handleLogOut}>
+                <Link to="/logout">Log Out</Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
         </ul>
 
-        <div className="hamburger md:hidden cursor-pointer" onClick={handleToggle}>
+        <div
+          className="hamburger md:hidden cursor-pointer"
+          onClick={handleToggle}
+        >
           <i
             className={
               toggleMenu
@@ -45,18 +83,34 @@ function Header() {
         </div>
 
         <ul className="desktop hidden md:flex gap-[1.5rem] text-white">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
+          {userInfo ? (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <div onClick={handleLogOut}>
+                <Link to="/logout">Log Out</Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </div>
