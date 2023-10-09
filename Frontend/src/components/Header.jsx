00a84c594368f259c50760e5db50601698d368bc
Jack <jackwebdev31@gmail.com>
2023-10-09 10:46:24 +0200

    react router done, starting redux

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  function handleToggle() {
+    setToggleMenu((prev) => !prev);
+  }
+
+  return (
+    <div className="bg-gray-700 relative">
+      <nav className="flex justify-between px-[2rem] py-[1rem]">
+        <Link className="text-white">Logo</Link>
+
+        <ul
+          className={
+            toggleMenu
+              ? "mobile grid gap-[1.5rem] text-center text-white bg-red-700 translate-y-[15%] -translate-x-[2rem] py-[2rem] w-full md:hidden absolute"
+              : "mobile grid gap-[1.5rem] text-center text-white bg-red-700 -translate-y-[150%] -translate-x-[2rem] py-[2rem] w-full md:hidden absolute"
+          }
+        >
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+
+        <div className="hamburger md:hidden" onClick={handleToggle}>
+          <i
+            className={
+              toggleMenu
+                ? "fa-solid fa-xmark text-white"
+                : "fa-solid fa-bars text-white"
+            }
+          ></i>
+        </div>
+
+        <ul className="desktop hidden md:flex gap-[1.5rem] text-white">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import store from './store.js'
+import { Provider } from "react-redux";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      {/* <Route path='' element={<PrivateRoute />}>
+      <Route path='/profile' element={<ProfileScreen />} />
+    </Route> */}
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
